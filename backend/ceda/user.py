dbd202c8ad08ecdfeb9a4da80e57afa09a29dc30
park jongheum <whdgmawkd@naver.com>
2023-05-25 20:42:30 +0900

    backend: User 파생 클래스 추가

--- backend/ceda/user.py
+++ backend/ceda/user.py
@@ -3,13 +3,29 @@ import enum
 
 class Role(enum.Enum):
     MODERATOR=0
-    POSITIVE_PARTICIPANT_1=1
-    POSITIVE_PARTICIPANT_2=2
-    NEGATIVE_PARTICIPANT_1=3
-    NEGATIVE_PARTICIPANT_2=4
-    GUEST=5
+    POSITIVE_PARTICIPANT=1
+    NEGATIVE_PARTICIPANT=2
+    LISTENER=3
 
 class User:
-    def __init__(self, role:Role = Role.GUEST) -> None:
+    def __init__(self, role:Role = Role.LISTENER) -> None:
         self.role = role
         pass
+
+class Moderator(User):
+    def __init__(self) -> None:
+        super().__init__(role=Role.MODERATOR)
+        pass
+
+class Listener(User):
+    def __init__(self) -> None:
+        super().__init__(role=Role.LISTENER)
+        pass
+
+class PositiveParticipant(User):
+    def __init__(self) -> None:
+        super().__init__(role=Role.POSITIVE_PARTICIPANT)
+
+class NegativeParticipant(User):
+    def __init__(self) -> None:
+        super().__init__(role=Role.NEGATIVE_PARTICIPANT)
